f8536d54730a3ddc5fa1887db98ca0537dd8c904
amanzholtanat <amanzholtanat@gmail.com>
2022-11-08 12:57:51 +0600

    basic while loop

---
+++ src/whileLoop.java
@@ -0,0 +1,9 @@
+public class whileLoop {
+    public static void main(String[] args) {
+        int value = 0;
+        while(value<10){
+            System.out.println("Hello World");
+            value++;
+        }
+    }
+}
