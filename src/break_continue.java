c3c88480ac2a2ab0d4a203f5102b34110192b261
amanzholtanat <amanzholtanat@gmail.com>
2022-11-09 19:03:03 +0600

    break

---
+++ src/break_continue.java
@@ -0,0 +1,13 @@
+public class break_continue {
+    public static void main(String[] args) {
+        int i=0;
+        while(true){
+            if(i>15){
+                break;
+            }
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("Мы вышли из цикла");
+    }
+}
