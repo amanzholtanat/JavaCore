5912907ad2ddaeebdb738cf01fa79d0d7930f122
amanzholtanat <amanzholtanat@gmail.com>
2022-11-10 10:46:01 +0600

    added Continue

--- src/break_continue.java
+++ src/break_continue.java
@@ -1,13 +1,11 @@
 public class break_continue {
     public static void main(String[] args) {
-        int i=0;
-        while(true){
-            if(i>15){
-                break;
+        int i;
+        for(i=0; i<=15; i++){
+         if(i%2==0){
+             continue;
             }
-            System.out.println(i);
-            i++;
+            System.out.println("Это нечетное число - " + i);
         }
-        System.out.println("Мы вышли из цикла");
     }
 }
