190e148fec1462596640f984c6d6190cb6bd0042
amanzholtanat <amanzholtanat@gmail.com>
2022-11-11 13:46:04 +0600

    Arrays

--- src/Arrays.java
+++ src/Arrays.java
@@ -2,15 +2,10 @@ public class Arrays {
     public static void main(String[] args) {
         int[] numbers = new int[5];
         for(int i = 0; i<numbers.length; i++){
-            numbers[i] = i*10;
+            numbers[i] = i;
         }
-        for(int i = 0; i<numbers.length; i++){
+        for(int i = 0; i< numbers.length; i++){
             System.out.println(numbers[i]);
-        }
-        System.out.println();
-        int[] numbers1 = {1,2,3};
-        for(int i = 0; i< numbers1.length; i++){
-            System.out.println(numbers1[i]);
         }
     }
 }
